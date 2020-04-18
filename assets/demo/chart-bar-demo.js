37372e2d8d112fb16ddc14c85804ecbc66ffc8cb
Nurul Amin Limon <nurulaminlimon261893@gmail.com>
2020-04-18 14:31:23 +0600

    chartbar dynamic with Api

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -4,43 +4,60 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
+$(document).ready(function () {
+
+    $.ajax({
+        url: 'https://www.fastaar.com/api',
+        type: 'get',
+        dataType: 'json',
+        cache: 'false',
+        success: function (res) {
+
+            console.log(res);
+            var myLineChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ["১-১০", "১০-২০", "২০-৩০", "৩০-৪০", "৪০-৫০", "৫০-৬০", "৬০+"],
+                    datasets: [{
+                        label: "Revenue",
+                        backgroundColor: "rgba(2,117,216,1)",
+                        borderColor: "rgba(2,117,216,1)",
+                        data: [parseInt(res.ages.onetoten), parseInt(res.ages.tentotwenty), parseInt(res.ages.twentytothirty), parseInt(res.ages.thirtytoforty), parseInt(res.ages.fortytofifty), parseInt(res.ages.fiftytosixty), parseInt(res.ages.sixtyplus)],
+                    }],
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            time: {
+                                unit: 'Age'
+                            },
+                            gridLines: {
+                                display: false
+                            },
+                            ticks: {
+                                maxTicksLimit: 7
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                min: 0,
+                                max: 160,
+                                maxTicksLimit: 20
+                            },
+                            gridLines: {
+                                display: true
+                            }
+                        }],
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+
+
         }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});
+
+    });
+
+})
