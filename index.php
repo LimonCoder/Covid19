e053e61fa9eab222e488577b42310be3ba420039
Nurul Amin Limon <nurulaminlimon261893@gmail.com>
2020-04-20 13:06:00 +0600

    remove commented code.

--- index.php
+++ index.php
@@ -28,27 +28,6 @@
     <div class="col">
         <h5>Corona BD LIVE</h5>
     </div>
-
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button> -->
-
-    <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent" style="border: 1px solid red;">
-    Corona BD LIVE     -->
-    <!-- <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.fastaar.com/api" target="_self">API</a>
-            </li>
-
-
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <button class="btn btn-danger my-2 my-sm-0" type="submit">Login</button>
-        </form> -->
-    <!-- </div> -->
 </nav>
 
 
